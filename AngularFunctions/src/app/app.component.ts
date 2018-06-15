c07b96da9b2850cea11170531a0a2363fdc71310
Thiago Passos <thiago@passos.com.au>
2018-06-16 03:13:37 +1000

    updating angular app to query from function

--- AngularFunctions/src/app/app.component.ts
+++ AngularFunctions/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  result;
+  error;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('/api/CrashAndLog').subscribe(result => {
+      this.result = result;
+    }, error => {
+      this.error = error;
+    })
+  }
 }
